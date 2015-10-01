876b7ccd22e12a0f93d12a25790533c0fd9d6542
Daniel Hartnel & Charles Mcmahon <student@epicodus.com>
2015-09-30 20:49:00 -0700

    add initial button functionality

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Hello from webpage!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Bye from webpage!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Stop copying yourself!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
